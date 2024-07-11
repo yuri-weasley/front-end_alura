7ada9ad6204d0b6d5a893d49f09987fe023276b2
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-07-11 08:46:42 -0400

    melhorando a acessibilidade do aside

--- 3486-acessibilidade-vidflow-projeto-base/js/botaoExpandirCanais.js
+++ 3486-acessibilidade-vidflow-projeto-base/js/botaoExpandirCanais.js
@@ -9,15 +9,17 @@ botaoExpandirCanais.addEventListener("click", () => {
     canal.classList.toggle("escondido");
   });
 
-  if (!canaisEstaoExpandidos) {
-    canaisEstaoExpandidos = true;
+  if (botaoExpandirCanais.ariaExpanded === "false") {
+    botaoExpandirCanais.ariaExpanded = true;
 
     botaoExpandirCanais.innerHTML = `
       <i class="icone__mostrar icone__mostrar-rotacionado"></i>
       <span class="texto-colapsar-canais">Mostrar menos</span>
     `;
+
+    canaisExtras[0].querySelector('a').focus();
   } else {
-    canaisEstaoExpandidos = false;
+    botaoExpandirCanais.ariaExpanded = false;
 
     botaoExpandirCanais.innerHTML = `
       <i class="icone__mostrar"></i>

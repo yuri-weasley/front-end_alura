76f6dea73cdcfbd4d2b8c048ceb786966c8c620e
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-07-11 09:23:02 -0400

    padrão de guias

--- 3486-acessibilidade-vidflow-projeto-base/js/botoesCategorias.js
+++ 3486-acessibilidade-vidflow-projeto-base/js/botoesCategorias.js
@@ -4,10 +4,18 @@ botoesCategorias.forEach((botao) => {
   botao.addEventListener("click", () => {
     const categoriaSelecionada = botao.getAttribute("name");
 
+    associarPainel(categoriaSelecionada);
     filtrarPorCategoria(categoriaSelecionada);
     atualizarEstadosDosBotoes(categoriaSelecionada);
   });
 });
+
+function associarPainel(categoriaSelecionada) {
+  const painelVideos = document.querySelector('[role = "tabpanel"]');
+  const idBotao = document.querySelector(`[name="${categoriaSelecionada}"]`).id;
+
+  painelVideos.setAttribute("aria-labelledby", idBotao);
+}
 
 function filtrarPorCategoria(filtro) {
   const videos = document.querySelectorAll(".videos__item");
@@ -26,6 +34,7 @@ function atualizarEstadosDosBotoes(categoriaSelecionada) {
   botoesCategorias.forEach((botao) => {
     const botaoFoiSelecionado = botao.getAttribute("name") === categoriaSelecionada;
 
-    botao.classList.toggle("selecionado", botaoFoiSelecionado);
+    //botao.classList.toggle("selecionado", botaoFoiSelecionado);
+    botao.ariaSelected = botaoFoiSelecionado;
   })
 }

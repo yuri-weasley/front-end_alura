83c04b870441bcc86ef84b3462095cd8c326987a
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-07-12 17:47:35 -0400

    configurando o watcher do TS para monitorar os arquivos TS

--- formacao_typescript-projeto-curso01-main/dist/js/bytebank.js
+++ formacao_typescript-projeto-curso01-main/dist/js/bytebank.js
@@ -1,18 +1,19 @@
 var saldo = 3000;
-var elementoSaldo = document.querySelector('.saldo-valor .valor');
+alert('Outra frase aqui');
+const elementoSaldo = document.querySelector('.saldo-valor .valor');
 if (elementoSaldo != null) {
     elementoSaldo.textContent = saldo.toString();
 }
-var elementoFormulario = document.querySelector('.block-nova-transacao form');
+const elementoFormulario = document.querySelector('.block-nova-transacao form');
 elementoFormulario.addEventListener('submit', function (event) {
     event.preventDefault();
     if (!elementoFormulario.checkValidity()) {
         alert("Por favor, preencha todos os campos da transação");
         return;
     }
-    var inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao');
-    var inputValor = elementoFormulario.querySelector('#valor');
-    var inputData = elementoFormulario.querySelector('#data');
+    const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao');
+    const inputValor = elementoFormulario.querySelector('#valor');
+    const inputData = elementoFormulario.querySelector('#data');
     var tipoTransacao = inputTipoTransacao.value;
     var valor = inputValor.valueAsNumber;
     var data = new Date(inputData.value);
@@ -27,7 +28,7 @@ elementoFormulario.addEventListener('submit', function (event) {
         return;
     }
     elementoSaldo.textContent = saldo.toString();
-    var novaTransacao = {
+    const novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data,
--- formacao_typescript-projeto-curso01-main/src/bytebank.ts
+++ formacao_typescript-projeto-curso01-main/src/bytebank.ts
@@ -1,5 +1,6 @@
 var saldo = 3000;
 
+alert('Outra frase aqui');
 const elementoSaldo = document.querySelector('.saldo-valor .valor') as HTMLElement;
 if(elementoSaldo != null ) {
     elementoSaldo.textContent = saldo.toString();

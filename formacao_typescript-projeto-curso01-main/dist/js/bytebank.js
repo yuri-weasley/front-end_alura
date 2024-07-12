b766d3118720433a0739e307eeb7dd6075502b29
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-07-12 16:25:02 -0400

    transações implementadas
    erro no depósito pois o JS recebe do form uma string e queremos somar com um valor

--- formacao_typescript-projeto-curso01-main/dist/js/bytebank.js
+++ formacao_typescript-projeto-curso01-main/dist/js/bytebank.js
@@ -10,4 +10,32 @@ elementoFormulario.addEventListener('submit', function(event) {
         alert("Por favor, preencha todos os campos da transação");
         return;
     }
+
+    const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao');
+    const inputValor = elementoFormulario.querySelector('#valor');
+    const inputData = elementoFormulario.querySelector('#data');
+
+    var tipoTransacao = inputTipoTransacao.value;
+    var valor = inputValor.value;
+    var data = inputData.value;
+
+    if(tipoTransacao == "Depósito") {
+        saldo += valor;
+    } else if(tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
+        saldo -= valor;
+    } else {
+        alert("Tipo de transação é inválida!");
+        return;
+    }
+
+    elementoSaldo.textContent = saldo;
+
+    const novaTransacao = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data
+    }
+
+    console.log(novaTransacao);
+    elementoFormulario.reset();
 });

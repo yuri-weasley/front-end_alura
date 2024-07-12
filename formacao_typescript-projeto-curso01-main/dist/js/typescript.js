95ee4125647aef900f78d51a877e266f45173ca9
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-07-12 18:10:01 -0400

    controle de tipagem

---
+++ formacao_typescript-projeto-curso01-main/dist/js/typescript.js
@@ -0,0 +1,20 @@
+var valor = 3000;
+var nome = "";
+var isPago = false;
+var qualquer = "";
+qualquer = 22;
+//Arrays
+const lista = [];
+lista.push(13, 22.5, 12, 89, 1.58);
+//Enum
+var TipoTransacao;
+(function (TipoTransacao) {
+    TipoTransacao["DEPOSITO"] = "Dep\u00F3sito";
+    TipoTransacao["TRANSFERENCIA"] = "Transfer\u00EAncia";
+    TipoTransacao["PAGAMENTO_BOLETO"] = "Pagamento de Boleto";
+})(TipoTransacao || (TipoTransacao = {}));
+const novaTransacao = {
+    tipoTransacao: TipoTransacao.PAGAMENTO_BOLETO,
+    data: new Date(),
+    valor: 0
+};
---
+++ formacao_typescript-projeto-curso01-main/src/typescript.ts
@@ -0,0 +1,30 @@
+var valor: number = 3000;
+var nome: string = "";
+var isPago: boolean = false;
+var qualquer: any = "";
+qualquer = 22;
+
+//Arrays
+
+const lista: number[] = [];
+lista.push(13, 22.5, 12, 89,1.58);
+
+//Tipos Personalizados (Type Alias)
+type Transacao = {
+    tipoTransacao: TipoTransacao;
+    data: Date;
+    valor: number;
+}
+
+//Enum
+enum TipoTransacao {
+    DEPOSITO = "Depósito",
+    TRANSFERENCIA = "Transferência",
+    PAGAMENTO_BOLETO = "Pagamento de Boleto"
+}
+
+const novaTransacao: Transacao = {
+    tipoTransacao: TipoTransacao.PAGAMENTO_BOLETO,
+    data: new Date(),
+    valor: 0
+}

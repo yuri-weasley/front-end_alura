65e032304da6edefb68d34734424d924fb95723e
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-07-12 16:44:34 -0400

    refatorando com ts

---
+++ formacao_typescript-projeto-curso01-main/dist/js/bytebank.ts
@@ -0,0 +1,44 @@
+var saldo = 3000;
+
+const elementoSaldo = document.querySelector('.saldo-valor .valor') as HTMLElement;
+if(elementoSaldo != null ) {
+    elementoSaldo.textContent = saldo.toString();
+}
+
+const elementoFormulario = document.querySelector('.block-nova-transacao form') as HTMLFormElement;
+elementoFormulario.addEventListener('submit', function(event) {
+    event.preventDefault();
+    if(!elementoFormulario.checkValidity()) {
+        alert("Por favor, preencha todos os campos da transação");
+        return;
+    }
+
+    const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao') as HTMLSelectElement;
+    const inputValor = elementoFormulario.querySelector('#valor') as HTMLInputElement;
+    const inputData = elementoFormulario.querySelector('#data') as HTMLInputElement;
+
+    var tipoTransacao: string = inputTipoTransacao.value;
+    var valor: number = inputValor.valueAsNumber;
+    var data: Date = new Date(inputData.value);
+
+    if(tipoTransacao == "Depósito") {
+        saldo += valor;
+    } else if(tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
+        saldo -= valor;
+    } else {
+        alert("Tipo de transação é inválida!");
+        return;
+    }
+
+    elementoSaldo.textContent = saldo.toString();
+
+    const novaTransacao = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data,
+        isNova: true
+    }
+
+    console.log(novaTransacao);
+    elementoFormulario.reset();
+});

15a0f78252d9da1e218068212074e691889a8401
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-07-14 13:17:37 -0400

    lançamento e captura de erros

--- formacao_typescript-projeto-curso01-main/src/types/Conta.ts
+++ formacao_typescript-projeto-curso01-main/src/types/Conta.ts
@@ -3,25 +3,42 @@ import { TipoTransacao } from "./TipoTransacao.js";
 
 var saldo: number = 3000;
 
+function debitar(valor: number): void {
+    if(valor <= 0) {
+        throw new Error("O valor a ser debitado deve ser maior que zero!");
+    }
+    if (valor > saldo) {
+        throw new Error("Saldo insuficiente!");
+    }
+
+    saldo -= valor;
+}
+function depositar(valor: number): void {
+    if(valor <= 0) {
+        throw new Error("O valor a ser depositado deve ser maior que zero!");
+        
+    }
+    saldo += valor;
+}
+
 const Conta = {
     getSaldo() {
         return saldo;
     },
-    
+
     getDataAcesso(): Date {
         return new Date();
     },
 
     registrarTransacao(novaTransacao: Transacao): void {
         if(novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
-            saldo += novaTransacao.valor;
+            depositar(novaTransacao.valor);
         }
         else if(novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA || novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
-            saldo -= novaTransacao.valor;
+            debitar(novaTransacao.valor);
         }
         else {
-            alert("Tipo de transação é inválida!");
-            return;
+            throw new Error("Tipo de transação é inválida!");
         }   
         
         console.log(novaTransacao);

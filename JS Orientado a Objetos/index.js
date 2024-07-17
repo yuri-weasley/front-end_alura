d9c7ece0d45fd4457a0649c1a9ac0b3d4498fde4
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-07-17 13:05:17 -0400

    oo

--- JS Orientado a Objetos/index.js
+++ JS Orientado a Objetos/index.js
@@ -1,21 +1,38 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
-    saldo;
+    #saldo;
+
+    sacar(valor) {
+        if(this.#saldo >= valor){
+            this.#saldo -= valor;
+        }        
+    }
+
+    depositar(valor) {
+        if(valor > 0) {
+            this.#saldo += valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 10000;
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.sacar(50);
+
+console.log(contaCorrenteRicardo.saldo);

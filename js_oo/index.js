51ade554177fa56a891b7ced512a995441b4f8e0
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-07-18 16:27:24 -0400

    modularizando o projeto

--- js_oo/index.js
+++ js_oo/index.js
@@ -1,27 +1,5 @@
-class Cliente {
-    nome;
-    cpf;
-}
-
-class ContaCorrente {
-    agencia;
-    //#saldo = 0 https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
-
-    sacar(valor) {
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }        
-    }
-
-    depositar(valor) {
-        if(valor <= 0) {
-            return;
-        }
-        this._saldo += valor;
-    }
-}
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";

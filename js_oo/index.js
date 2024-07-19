5c4ef59dffa5f89d36b6254f475f3a037507ef01
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-07-18 20:42:08 -0400

    Getters e Setters

--- js_oo/index.js
+++ js_oo/index.js
@@ -5,9 +5,9 @@ const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-//const cliente2 = new Cliente();
-//cliente2.nome = "Alice";
-//cliente2.cpf = 88822233309;
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
@@ -15,11 +15,11 @@ contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = new Cliente();
-conta2.cliente.nome = "Alice";
-conta2.cliente.cpf = 88822233309;
+conta2.cliente = cliente1;
 conta2.agencia = 102;
 
 let valor = 200;
 contaCorrenteRicardo.transferir(valor, conta2);
-console.log(conta2);
+
+conta2.saldo = 30000;
+console.log(conta2.saldo);

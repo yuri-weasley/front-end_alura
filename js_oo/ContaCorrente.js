746fdff0ef78c8860cfcb24f2de111659311605d
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-07-19 15:31:56 -0400

    atributos estáticos

--- js_oo/ContaCorrente.js
+++ js_oo/ContaCorrente.js
@@ -1,6 +1,7 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente {
+    static numeroDeContas = 0;
     agencia;
     _cliente;
       //#saldo = 0 https://github.com/tc39/proposal-class-fields#private-fields
@@ -22,9 +23,10 @@ export class ContaCorrente {
         return this._saldo;
     }
 
-    constructor(cliente, agencia) {
+    constructor(agencia, cliente) {
         this.agencia = agencia;
         this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
     }
 
     sacar(valor) {

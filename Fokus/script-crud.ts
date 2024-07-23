a84bb2d8480cb04e94312b42103d4257a700a006
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-07-23 09:11:05 -0400

    install npm ts

--- Fokus/script-crud.ts
+++ Fokus/script-crud.ts
@@ -36,10 +36,10 @@ const selecionarTarefa = (estado: EstadoAplicacao, tarefa: Tarefa): EstadoAplica
 const atualizarUI = () => {
 
     const taskIconSvg = `
-            <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF" />
             <path
-            d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.406L4. fill="#01080E" />
+            d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.406L4.82812 12L9 16.1719Z fill="#01080E" />
         </svg>
     `
 

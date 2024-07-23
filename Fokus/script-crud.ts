d21cc84b73323d0e1f612db7c95283671c942850
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-07-23 08:49:07 -0400

    dps continuo

--- Fokus/script-crud.ts
+++ Fokus/script-crud.ts
@@ -34,12 +34,47 @@ const selecionarTarefa = (estado: EstadoAplicacao, tarefa: Tarefa): EstadoAplica
 }
 
 const atualizarUI = () => {
+
+    const taskIconSvg = `
+            <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF" />
+            <path
+            d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.406L4. fill="#01080E" />
+        </svg>
+    `
+
     const ulTarefas = document.querySelector('.app__section-task-list')
     if(ulTarefas) {
         ulTarefas.innerHTML = ''
     }
 
-    estadoInicial.tarefas.forEach(tarefas => {
-        
+    estadoInicial.tarefas.forEach(tarefa => {
+        const li = document.createElement('li')
+        li.classList.add('app__section-task-list-item')
+        const SvgIcon = document.createElement('svg')
+        SvgIcon.innerHTML = taskIconSvg
+
+        const paragraph = document.createElement('p')
+        paragraph.classList.add('app__section-task-list')
+        paragraph.textContent = tarefa.descricao
+
+        const button = document.createElement('button')
+        button.classList.add('app_button-edit')
+
+        const editIcon = document.createElement('')
+        editIcon.setAttribute('src', '/imagens/edit.png')
+
+        button.appendChild(editIcon)
+
+        if(tarefa.concluida) {
+            button.setAttribute('disabled', 'true')
+            li.classList.add('app__section-task-list-item-complete')
+        }
+
+        li.appendChild(SvgIcon)
+        li.appendChild(paragraph)
+        li.appendChild(button)
+
+        ulTarefas?.appendChild(li)
     })
 }

4b6ececaea3caf5a01b517409d4860e97b54d617
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-07-28 16:06:40 -0400

    configurações do compilador e papel do arquivo tsconfig.json

--- typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -1,9 +1,14 @@
 export class Negociacao {
+    private _data;
+    private _quantidade;
+    private _valor;
+
     constructor(data, quantidade, valor) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
+
     get data() {
         return this._data;
     }

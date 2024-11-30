8239ea1b8e42fff65818336d596b97d678de37d9
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-11-30 10:58:13 -0400

    iterando com o @for do angular

--- indexa/indexa/src/app/app.component.ts
+++ indexa/indexa/src/app/app.component.ts
@@ -3,14 +3,21 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./componentes/container/container.component";
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { SeparadorComponent } from './componentes/separador/separador.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent, CabecalhoComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    ContainerComponent, 
+    CabecalhoComponent,
+    SeparadorComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
 }

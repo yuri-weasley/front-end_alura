f16555aea706b697278e1aa1560e171d8b0e4822
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-12-01 19:37:23 -0400

    update files

--- indexa/indexa/src/app/app.component.ts
+++ indexa/indexa/src/app/app.component.ts
@@ -4,17 +4,19 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./componentes/container/container.component";
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from "./componentes/contato/contato.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule, 
-    RouterOutlet, 
-    ContainerComponent, 
+    CommonModule,
+    RouterOutlet,
+    ContainerComponent,
     CabecalhoComponent,
-    SeparadorComponent
-  ],
+    SeparadorComponent,
+    ContatoComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

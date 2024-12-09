c6c8a85fc3887d875ba0bab4c05c3535ff56138e
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-12-09 08:54:41 -0400

    form orientado a dados

--- indexa/src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ indexa/src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from "../../componentes/separador/separador.component";
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
@@ -9,11 +10,22 @@ import { SeparadorComponent } from "../../componentes/separador/separador.compon
   imports: [
     CommonModule,
     ContainerComponent,
-    SeparadorComponent
+    SeparadorComponent,
+    ReactiveFormsModule
 ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
-
+  contatoForm!: FormGroup; //operador de asserção não nula, garanto q vou inicializar essa propriedade
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('Fê'),
+      telefone: new FormControl(''),
+      email: new FormControl(''),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl('')
+    })
+  }
 }

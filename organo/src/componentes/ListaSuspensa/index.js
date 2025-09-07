616e8ce036f053fb31158de0dd0aa319fc5bd786
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2025-09-06 20:14:37 -0400

    Uso do useState

--- organo/src/componentes/ListaSuspensa/index.js
+++ organo/src/componentes/ListaSuspensa/index.js
@@ -1,12 +1,11 @@
 import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
-    console.log(props.itens)
 
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select required={props.obrigatorio}>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.obrigatorio} value={props.valor}>
                 {props.item.map(item => {
                     return <option key={item}>{item}</option>
                 })}

bd4124d1faf65e3678551eb244e20d452d788955
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2025-09-08 20:04:26 -0400

    finished organo

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -1,10 +1,14 @@
+import { Fragment } from 'react/jsx-runtime'
 import './Banner.css'
 
 export const Banner = () => {
     // JSX: JavaScript XML
     return (
-        <header className="banner">
+        <> {/* Fragment */}
+            <header className="banner">
             <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
         </header>
+        <h1>Teste</h1>
+        </>
     )
 }
--- organo/src/componentes/ListaSuspensa/index.js
+++ organo/src/componentes/ListaSuspensa/index.js
@@ -6,6 +6,7 @@ const ListaSuspensa = (props) => {
         <div className='lista-suspensa'>
             <label>{props.label}</label>
             <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.obrigatorio} value={props.valor}>
+                <option value=''></option>
                 {props.item.map(item => {
                     return <option key={item}>{item}</option>
                 })}

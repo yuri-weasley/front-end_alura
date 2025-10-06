f0bcee87907bf43140a4d1aa3692d17b0a6247be
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2025-10-06 11:43:40 -0400

    Fim do projeto ola-mundo

--- ola-mundo/src/routes.js
+++ ola-mundo/src/routes.js
@@ -6,17 +6,18 @@ import Rodape from "./componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
 import Post from "paginas/Post";
 import NaoEncontrada from "paginas/NaoEncontrada";
+import ScrollToTop from "componentes/ScrollToTop";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
+    <ScrollToTop/>
     <Menu/>
 
       <Routes>
         <Route path="/" element={<PaginaPadrao/>}>
           <Route index element={<Inicio/>} />
           <Route path="sobremim" element={<SobreMim/>} />
-          <Route path="posts/:id" element={<Post/>}/>
         </Route>
 
         {/* 
@@ -31,6 +32,7 @@ function AppRoutes() {
           </PaginaPadrao>
         */}
 
+        <Route path="posts/:id/*" element={<Post/>}/>
         <Route path="*" element={<NaoEncontrada/>} />
       </Routes>
 

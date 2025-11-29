5af68b2a82e76c12fbd75879b303b6cdc5cdd6a7
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2025-11-29 13:29:12 -0400

    Explorando a Context API

--- meteora/src/context/CarrinhoContext.jsx
+++ meteora/src/context/CarrinhoContext.jsx
@@ -3,8 +3,10 @@ import { createContext } from "react";
 export const CarrinhoContext = createContext();
 
 export const CarrinhoProvider = ({children}) => {
+    const {carrinho, setCarrinho} = useState([]);
+
     return (
-        <CarrinhoContext.Provider>
+        <CarrinhoContext.Provider value={{carrinho, setCarrinho}}>
             {children}
         </CarrinhoContext.Provider>
     )
--- meteora/src/pages/Home.jsx
+++ meteora/src/pages/Home.jsx
@@ -9,28 +9,16 @@ import Categorias from "@/components/Categorias";
 import Facilidades from "@/components/Facilidades";
 import Novidades from "@/components/Novidades";
 
-const Home = ({
-  carrinho,
-  adicionarProduto,
-  removerProduto,
-  removerProdutoCarrinho,
-  valorTotalCarrinho,
-  quantidadeProdutos,
-}) => {
+const Home = () => {
   return (
     <>
-      <BarraNavegacao quantidadeProdutos={quantidadeProdutos} />
+      <BarraNavegacao />
       <CarrinhoSuspenso
-        carrinho={carrinho}
-        adicionarProduto={adicionarProduto}
-        removerProduto={removerProduto}
-        removerProdutoCarrinho={removerProdutoCarrinho}
-        valorTotalCarrinho={valorTotalCarrinho}
       />
       <main>
         <Carrossel />
         <Categorias />
-        <Produtos adicionarProduto={adicionarProduto} />
+        <Produtos />
         <Facilidades />
         <Novidades />
       </main>
